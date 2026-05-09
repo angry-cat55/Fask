4c571e671e9e1975534378e3f3d24e65e5d033fe
Yu Jihwan <applewlghks321@gmail.com>
2026-05-09 20:03:28 +0900

    fix: 회원가입 API 경로 명시화 및 라우팅 엄격성 설정

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/userController');
+const userController = require('../controllers/userController');
 
 // /api/users 요청을 userController의 signup 컨트롤러로 전달
-router.post('', userController.signup);
+router.post('/', userController.signup);
 
 module.exports = router;
