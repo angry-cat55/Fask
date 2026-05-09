cf5addff3ef070490f9bbbd2dbeec9ee40b05bce
aihonte <c01040593597@gmail.com>
2026-05-09 14:35:54 +0900

    refactor: 폴더 구조 정리 (components -> pages)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './component/Login/Login.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './pages/Login/Login.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
