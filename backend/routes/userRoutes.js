2d2ef63dca9f0608073cae666007b00225c9b5c0
Yu Jihwan <applewlghks321@gmail.com>
2026-05-11 20:02:34 +0900

    feat: 아이디 중복 확인 API 구현 및 테스트 응답 코드 작성

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
+// 회원가입 요청 API
 // /api/users 요청을 userController의 signup 컨트롤러로 전달
 router.post('/', userController.signup);
+
+// 닉네임 중복 확인 요청 API
+// /api/users/check-username 요청을 userController의 checkUsername 컨트롤러로 전달
+router.get('/check-username', userController.checkUsername);
 
 module.exports = router;
