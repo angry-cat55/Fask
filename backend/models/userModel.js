f2b8431df7d25061eba969628ab1b169dfa5b592
Yu Jihwan <applewlghks321@gmail.com>
2026-05-13 18:37:25 +0900

    feat: 구현된 API의 service와 model 기본 틀 구현

---
+++ backend/models/userModel.js
@@ -0,0 +1,9 @@
+exports.createUser = async (userData) => {
+    // TODO: 회원 정보 저장 SQL 작성
+    return null;
+};
+
+exports.findUserByLoginId = async (loginId) => {
+    // TODO: loginId 중복 확인용 조회 SQL 작성
+    return null;
+};
