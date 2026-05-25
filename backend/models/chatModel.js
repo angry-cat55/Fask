269c2494d6ad2eb324e2489b5b75a59714392a4c
Yu Jihwan <applewlghks321@gmail.com>
2026-05-25 19:17:01 +0900

    fix: 테이블명 오타 수정

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -3,7 +3,7 @@ const pool = require('../config/db');
 // 새 메세지 저장
 exports.saveMessage = async (workspaceId, userId, content) => {
     const sql = `
-        INSERT INTO messages (workspace_id, user_id, content)
+        INSERT INTO chat_messages (workspace_id, user_id, content)
         VALUES (?, ?, ?)
     `;
     const [result] = await pool.execute(sql, [workspaceId, userId, content]);
@@ -11,8 +11,8 @@ exports.saveMessage = async (workspaceId, userId, content) => {
 
     // messageId, workspaceId, userId, nickname, content, sendAt이 포함된 메세지 객체 반환
     const [rows] = await pool.execute(`
-        SELECT m.message_id AS messageId, m.workspace_id AS workspaceId, m.user_id AS userId, u.nickname, m.content, m.created_at AS sendAt
-        FROM messages m
+        SELECT m.message_id AS messageId, m.workspace_id AS workspaceId, m.user_id AS userId, u.nickname, m.content, m.send_at AS sendAt
+        FROM chat_messages m
         JOIN users u ON m.user_id = u.user_id
         WHERE m.message_id = ?
     `, [messageId]);
